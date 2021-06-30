5eef887faf7b7f937ecf580c6f48e40265f2c52b
Luke Marsden <me@lukemarsden.net>
2021-06-30 16:20:30 +0100

    install script

--- scripts/hack-my-compute.sh
+++ scripts/hack-my-compute.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo "todo: set up rslex on the AML compute here"
+echo "Installing kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+echo "Installing Microsoft's dev version of dataprep for py36"
+/anaconda/envs/azureml_py36/bin/pip install --force-reinstall --extra-index-url https://dataprepdownloads.azureedge.net/pypi/test-M3ME5B1GMEM3SW0W/38723857/ azureml-dataprep==2.18.0.dev0+98293a5
+
+echo "Installing Pachyderm's version of rslex for py36"
+curl -LO https://github.com/pachyderm/aml/raw/main/dist/azureml_dataprep_rslex-1.16.0.dev0%2B329a308-cp36-cp36m-linux_x86_64.whl
+/anaconda/envs/azureml_py36/bin/pip install azureml_dataprep_rslex-1.16.0.dev0%2B329a308-cp36-cp36m-linux_x86_64.whl
+
+#echo "Installing Microsoft's dev version of dataprep for py38"
+#/anaconda/envs/azureml_py38/bin/pip install --force-reinstall --extra-index-url https://dataprepdownloads.azureedge.net/pypi/test-M3ME5B1GMEM3SW0W/38723857/ azureml-dataprep==2.18.0.dev0+98293a5
+
+#echo "Installing Pachyderm's version of rslex for py38"
+#curl -LO https://github.com/pachyderm/aml/raw/main/dist/azureml_dataprep_rslex-1.16.0.dev0%2B329a308-cp38-cp38m-linux_x86_64.whl
+#/anaconda/envs/azureml_py38/bin/pip install azureml_dataprep_rslex-1.16.0.dev0%2B329a308-cp38-cp38m-linux_x86_64.whl
