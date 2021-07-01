ce5c70911789db5ba6e99e777c98394c1069b883
Albert Cui <albert.cui@pachyderm.io>
2021-07-01 16:17:42 +0000

    release 507d32b py38

--- scripts/install-rslex-pachyderm-beta.sh
+++ scripts/install-rslex-pachyderm-beta.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PY36_VERSION=507d32b
-export PY38_VERSION=329a308
+export PY38_VERSION=507d32b
 
 echo "Installing kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
