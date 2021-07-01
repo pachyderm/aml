27c9b5615ab8e9949e257f8d7958e2327b7cbd19
Albert Cui <albscui@gmail.com>
2021-07-01 09:29:34 -0700

    use random string for everything

--- terraform/pachyderm.tf
+++ terraform/pachyderm.tf
@@ -1,7 +1,7 @@
 // Storage for pachyderm cluster
 
 resource "azurerm_storage_account" "pachyderm" {
-  name                     = "pachyderm${random_id.deployment.hex}"
+  name                     = "pachyderm-${random_id.deployment.hex}"
   resource_group_name      = local.resource_group_name
   location                 = local.resource_group_location
   account_tier             = "Standard"
@@ -9,16 +9,7 @@ resource "azurerm_storage_account" "pachyderm" {
 }
 
 resource "azurerm_storage_container" "pachyderm" {
-  name                  = "pachyderm"
+  name                  = "pachyderm-${random_id.deployment.hex}"
   storage_account_name  = azurerm_storage_account.pachyderm.name
   container_access_type = "private"
 }
-
-/*
-output "storage_access_key" {
-  value = azurerm_storage_account.pachyderm.primary_access_key
-}
-output "storage_account_name" {
-  value = azurerm_storage_account.pachyderm.name
-}
-*/
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,3 @@
-// TODO: pass the prefix into everything
-
-variable "prefix" {
-  description = "The prefix which should be used for all resources in this example"
-  default = "example"
-}
-
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
   default = "West Europe"
