09e3a9a7a51749fd12a40092205d718c47642807
Albert Cui <albert.cui@pachyderm.io>
2021-07-01 15:39:10 +0000

    release new py36 version of rslex

--- scripts/install-rslex-pachyderm-beta.sh
+++ scripts/install-rslex-pachyderm-beta.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export PY36_VERSION=507d32b
+export PY38_VERSION=329a308
+
 echo "Installing kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
@@ -8,12 +11,12 @@ echo "Installing Microsoft's dev version of dataprep for py36"
 /anaconda/envs/azureml_py36/bin/pip install --force-reinstall --extra-index-url https://dataprepdownloads.azureedge.net/pypi/test-M3ME5B1GMEM3SW0W/38723857/ azureml-dataprep==2.18.0.dev0+98293a5
 
 echo "Installing Pachyderm's version of rslex for py36"
-curl -LO https://github.com/pachyderm/aml/raw/main/dist/azureml_dataprep_rslex-1.16.0.dev0%2B329a308-cp36-cp36m-linux_x86_64.whl
-/anaconda/envs/azureml_py36/bin/pip install azureml_dataprep_rslex-1.16.0.dev0%2B329a308-cp36-cp36m-linux_x86_64.whl
+curl -LO https://github.com/pachyderm/aml/raw/main/dist/azureml_dataprep_rslex-1.16.0.dev0%2B${PY36_VERSION}-cp36-cp36m-linux_x86_64.whl
+/anaconda/envs/azureml_py36/bin/pip install azureml_dataprep_rslex-1.16.0.dev0%2B${PY36_VERSION}-cp36-cp36m-linux_x86_64.whl
 
 echo "Installing Microsoft's dev version of dataprep for py38"
 /anaconda/envs/azureml_py38/bin/pip install --force-reinstall --extra-index-url https://dataprepdownloads.azureedge.net/pypi/test-M3ME5B1GMEM3SW0W/38723857/ azureml-dataprep==2.18.0.dev0+98293a5
 
 echo "Installing Pachyderm's version of rslex for py38"
-curl -LO https://github.com/pachyderm/aml/raw/main/dist/azureml_dataprep_rslex-1.16.0.dev0%2B329a308-cp38-cp38-linux_x86_64.whl
-/anaconda/envs/azureml_py38/bin/pip install azureml_dataprep_rslex-1.16.0.dev0%2B329a308-cp38-cp38-linux_x86_64.whl
+curl -LO https://github.com/pachyderm/aml/raw/main/dist/azureml_dataprep_rslex-1.16.0.dev0%2B${PY38_VERSION}-cp38-cp38-linux_x86_64.whl
+/anaconda/envs/azureml_py38/bin/pip install azureml_dataprep_rslex-1.16.0.dev0%2B${PY38_VERSION}-cp38-cp38-linux_x86_64.whl
