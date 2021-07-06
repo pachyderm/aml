1474ff1e3ded41c8e58cf50654596a9023a90eb4
Luke Marsden <me@lukemarsden.net>
2021-07-06 06:49:55 +0100

    split systemd startup script out into separate file

--- scripts/install.sh
+++ scripts/install.sh
@@ -47,31 +47,23 @@ until timeout 1s bash $SCRIPT_DIR/check_ready.sh app=pachd; do sleep 1; done
 
 kubectl get all
 
+echo "Waiting for 30 seconds for pachd to bind its ports before proceeding..."
+sleep 30
+
 cat << EOF | sudo tee /etc/systemd/system/pachyderm-aml-syncer.service
 [Unit]
 Description=Pachyderm AzureML Syncer
+
 [Service]
 User=ubuntu
 WorkingDirectory=/home/ubuntu
-ExecStart=/bin/bash -c " \
-    pkill -f 'port-forward' || true; \
-    echo \"Waiting for 30 seconds for pachd to bind its ports before proceeding...\"; \
-    sleep 30; \
-    kubectl port-forward service/pachd 30650:650 & \
-    pid=\$!; \
-    echo \"Waiting for 15 seconds for port forwarding to start working before proceeding...\"; \
-    sleep 15; \
-    if [ ! -e /proc/\$pid/cmdline ]; then; \
-        echo \"Port-forward exited, oh no\"; \
-        exit 1; \
-    fi; \
-    source /home/ubuntu/scripts/env.sh; \
-    /usr/bin/python3 /home/ubuntu/syncer/sync.py \
-"
+ExecStart=/home/ubuntu/scripts/start.sh
 Environment=PYTHONUNBUFFERED=1
 Restart=always
+
 [Install]
 WantedBy=multi-user.target
 EOF
+
 sudo systemctl daemon-reload
 sudo systemctl start pachyderm-aml-syncer
